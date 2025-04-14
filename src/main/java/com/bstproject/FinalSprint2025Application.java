f113cd9253370d785064181ad00233d3a254b2b6
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2025-04-13 22:37:12 -0230

    seeing if application runs

---
+++ src/main/java/com/bstproject/FinalSprint2025Application.java
@@ -0,0 +1,11 @@
+package com.bstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalSprint2025Application {
+    public static void main(String[] args) {
+        SpringApplication.run(FinalSprint2025Application.class, args);
+    }
+}
--- src/main/java/com/bstproject/model/TreeNode.java
+++ src/main/java/com/bstproject/model/TreeNode.java
@@ -1,5 +1,33 @@
-package main.java.com.bstproject.model;
+package com.bstproject.model;
 
 public class TreeNode {
-    
+    private int value;
+    private TreeNode left;
+    private TreeNode right;
+
+    public TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
