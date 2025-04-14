7991891ff546436b0fdf553cccd7bdd2262fbb36
RyanTibbo87 <Ryan.Tibbo@keyin.com>
2025-04-13 22:38:47 -0230

    tree data completed

--- src/main/java/com/bstproject/model/TreeData.java
+++ src/main/java/com/bstproject/model/TreeData.java
@@ -1,5 +1,47 @@
-package main.java.com.bstproject.model;
+package com.bstproject.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class TreeData {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String inputNumbers;
+    private String treeStructure;
+
+    public TreeData() {}
+
+    public TreeData(String inputNumbers, String treeStructure) {
+        this.inputNumbers = inputNumbers;
+        this.treeStructure = treeStructure;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInputNumbers() {
+        return inputNumbers;
+    }
+
+    public void setInputNumbers(String inputNumbers) {
+        this.inputNumbers = inputNumbers;
+    }
+
+    public String getTreeStructure() {
+        return treeStructure;
+    }
+
+    public void setTreeStructure(String treeStructure) {
+        this.treeStructure = treeStructure;
+    }
 }
